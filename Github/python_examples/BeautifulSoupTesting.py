7e6c491b3a57a716341960ce8ee83bb8d665f052
dh08loma <DanLo1108@Daniels-MacBook-Pro.local>
2013-10-02 22:29:09 -0400

    10/2

--- Github/python_examples/BeautifulSoupTesting.py
+++ Github/python_examples/BeautifulSoupTesting.py
@@ -2,26 +2,52 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import numpy
-from numpy import linalg, matrix
+from numpy import linalg, matrix, array
 import HTMLParser
 from HTMLParser import HTMLParser
 
+
+start=3
+
+
+
 r=requests.get('http://espn.go.com/nfl/statistics/player/_/stat/passing')
 
-data=r.content
+data1=r.content
 
-soup=BeautifulSoup(data)
-count=0
+soup=BeautifulSoup(data1)
 
 tables=soup.find_all('table')
-print tables
+
+a = ["" for x in range(1000)]
+global count
+count=0
 
 class MyHTMLParser(HTMLParser):
+    
     def handle_data(self,data):
-        print "data: ",data
-        #print len(data)
-        
+        global count
+        print data
+        a[count]=data
+        count = count + 1
+       
         
 parser=MyHTMLParser()
-print parser
 parser.feed(str(tables))
+b=parser.handle_data(str(tables))
+
+
+dataarray=["" for x in range(start,count-1)]
+for i in range(count-start-1):
+    dataarray[i]=a[i+start]
+
+
+
+
+
+
+
+
+
+
+

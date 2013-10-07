764bbe48a4e09e20df1b115610ad2151bf5ce571
dh08loma <DanLo1108@Daniels-MacBook-Pro.local>
2013-10-07 00:40:37 -0400

    10/6

--- Github/python_examples/BeautifulSoupTesting.py
+++ Github/python_examples/BeautifulSoupTesting.py
@@ -1,19 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import numpy
+import numpy as np
 from numpy import linalg, matrix, array
 import HTMLParser
 from HTMLParser import HTMLParser
 
-import numpy as np
-
 
 start=2
 
-
-
-r1=requests.get('http://www.nfl.com/stats/categorystats?archive=true&conference=null&role=TM&offensiveStatisticCategory=TEAM_PASSING&defensiveStatisticCategory=null&season=2012&seasonType=REG&tabSeq=2&qualified=false&Submit=Go')
+r1=requests.get('http://espn.go.com/nfl/statistics/team/_/stat/passing/year/2012')
 #r2.requests.get('http://www.nfl.com/stats/categorystats?archive=true&conference=null&role=TM&offensiveStatisticCategory=RUSHING&defensiveStatisticCategory=null&season=2012&seasonType=REG&tabSeq=2&qualified=false&Submit=Go')
 
 data1=r1.content
@@ -25,7 +21,7 @@ soup1=BeautifulSoup(data1)
 tables1=soup1.find_all('table')
 #tables2=soup2.find_all('table')
 
-a1 = ["" for x in range(1000)]
+a1 = ["" for x in range(6000)]
 global count
 count=0
 
@@ -33,7 +29,7 @@ class MyHTMLParser(HTMLParser):
     
     def handle_data(self,data):
         global count
-        print data
+        #print data
         a1[count]=data
         count = count + 1
        
@@ -45,29 +41,22 @@ b=parser.handle_data(str(tables1))
 
 dataarray=["" for x in range(start,count-1)]
 for i in range(count-start-1):
-    dataarray[i]=a[i+start]
+    dataarray[i]=a1[i+start]
+
+dataarray=array(dataarray)
 
 
-ncolumns = 14
-nvalues = len(dataarray)
+ncols=14
+nvalues=len(dataarray)-ncols
+for i in range(ncols):
+    if(dataarray[i]=='YDS'):
+        index=i
 
-index = np.arange(0,nvalues,ncolumns)
-print index
+indexes=np.arange(0,nvalues,ncols)
+datamatrix=matrix([[0.0]*ncols]*32)
 
-dataarray = array(dataarray)
-dataarray = dataarray[dataarray!='\xc2\xa0']
-print dataarray
-
-
-print dataarray[index]
-print dataarray[index+1]
-print dataarray[index+2]
-
-
-
-
-
-
-
+for i in range(ncols):
+    datamatrix[:,i]=dataarray[indexes+i]
+    
 
 
